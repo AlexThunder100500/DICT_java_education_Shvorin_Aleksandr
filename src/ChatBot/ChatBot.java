fa829d1cbc29d85a0c15aaaf0fd48c20662fc08c
AlexThunder100500 <avatar45665@gmail.com>
2021-12-20 20:29:13 +0200

    ChatBot 3-rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,4 +1,5 @@
 package ChatBot;
+
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -8,9 +9,28 @@ public class ChatBot {
         LocalDate localDate = LocalDate.now();
 
         System.out.println();
-        System.out.println("Hello! My name is Dima");
+        System.out.println("Hello! My name is  Dima");
         System.out.println("I was created in " + localDate);
         Scanner s11 = new Scanner(System.in);
         System.out.println("What a great name you have, " + s11.nextLine());
+
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        ChatBot chatBot = new ChatBot();
+        chatBot.calcAge(s11);
+
+
+
+
     }
+
+    private void calcAge(Scanner s11) {
+        int remainder7 = s11.nextInt();
+        int remainder5 = s11.nextInt();
+        int remainder3 = s11.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + age + " that's a good time to start programming!");
+
+    }
+
+
 }
