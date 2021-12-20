ca5cf679a61efb60eeec1129dff1e5e5ae6a504c
AlexThunder100500 <avatar45665@gmail.com>
2021-12-20 20:07:56 +0200

    ChatBot 2-st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,5 +1,6 @@
 package ChatBot;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
@@ -9,5 +10,7 @@ public class ChatBot {
         System.out.println();
         System.out.println("Hello! My name is Dima");
         System.out.println("I was created in " + localDate);
+        Scanner s11 = new Scanner(System.in);
+        System.out.println("What a great name you have, " + s11.nextLine());
     }
 }
